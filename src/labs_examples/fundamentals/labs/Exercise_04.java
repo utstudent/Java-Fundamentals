95f44ac833be20fbef408e9bda70000a5e97674e
utstudent <talhakaran@utexas.edu>
2022-07-24 14:25:40 -0500

    exercise 4&5 done

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -12,9 +12,29 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
+        boolean isCool = true;
+        System.out.println("Java is cool:" + isCool);
+
+        char letter = 'A';
+        System.out.println("The first letter in the alphabet: " + letter);
+
+        byte byteExample = 7;
+        System.out.println("My byte value: " + byteExample);
+
+        short shortExample = 10982;
+        System.out.println("My short value: " + shortExample);
 
         int i = 1;
         System.out.println("int i is: " + i);
+
+        long longExample = 981923l;
+        System.out.println("My long example: " + longExample);
+
+        float floatExample = 71.123f;
+        System.out.println("My float example: " + floatExample);
+
+        double doubleExample = 234.2348234;
+        System.out.println("My double example: " + doubleExample);
 
         // write your code below
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,20 +14,35 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
-        //int strLength = str.length;
+        int strLength = str.length();
+        System.out.println("length of str is: " + strLength);
 
         String str2 = "hello";
+
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean equal = str.equals(str2);
+        System.out.println("str is equal to str2: " + equal);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String str3 = str+str2;
+        System.out.println(str3);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
 
-        //System.out.println(strLength);
-        System.out.println("hi");
+        String test1 = str.replace("l", "t");
+        System.out.println(test1);
+
+        String test2 = str.substring(1, 5);
+        System.out.println(test2);
+
+        boolean containsTest = str2.contains("hell");
+        System.out.println("Does str2 contian hell? true or false: " + containsTest);
+
+        String mystr = "My name is Bob and I am builder";
+        int indexOfTest = mystr.indexOf("Bob");
+        System.out.println("Bobs index is: " + indexOfTest);
 
     }
-
 
 }
