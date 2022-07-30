2026b046eeebd85860c19ad3d485631619102533
utstudent <talhakaran@utexas.edu>
2022-07-29 22:05:40 -0500

    conditions loop exercise 2

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -1,5 +1,8 @@
 package labs_examples.conditions_loops.labs;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 2: Days of the week
  *
@@ -16,6 +19,30 @@ public class Exercise_02 {
     public static void main(String[] args) {
     
         // write completed here
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+        if (number >0) {
+            if (number == 1) {
+                System.out.println("Monday");
+            } else if (number == 2) {
+                System.out.println("Tuesday");
+            } else if (number == 3) {
+                System.out.println("Wednesday");
+            } else if (number == 4) {
+                System.out.println("Thursday");
+            } else if (number == 5) {
+                System.out.println("Friday");
+            } else if (number == 6) {
+                System.out.println("Saturday");
+            } else if (number == 7) {
+                System.out.println("Sunday");
+            } else {
+                System.out.println("Other");
+            }
+        } else {
+            System.out.println("Invalid number");
+        }
     }
 }
