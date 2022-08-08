fa66fa3c85aeebefcdf75ec90c32fb38425ac13a
utstudent <talhakaran@utexas.edu>
2022-08-07 23:21:58 -0500

    exercise 3

--- src/labs_examples/arrays/labs/Exercise_03.java
+++ src/labs_examples/arrays/labs/Exercise_03.java
@@ -3,7 +3,7 @@ package labs_examples.arrays.labs;
 /**
  *  2D Array
  *
- *      Creat and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
+ *      Create and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
  *
  *      The output should look something like this:
  *
@@ -18,6 +18,20 @@ package labs_examples.arrays.labs;
 public class Exercise_03 {
 
     public static void main(String[] args) {
-        
+
+        int [][] nums = new int [5][5];
+
+        for(int x = 0; x < nums.length; x++) {
+            for (int y = 0; y < nums[x].length; y++) {
+                nums[x][y] = (x+1)*(y+1)*3;
+            }
+        }
+
+        for (int[] outer : nums) {
+            for (int val : outer) {
+                System.out.print(val + "\t");
+            }
+            System.out.println();
+        }
     }
 }
