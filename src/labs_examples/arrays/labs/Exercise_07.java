03af21468edb3b44f471e790065e9f9623c6abc4
utstudent <talhakaran@utexas.edu>
2022-08-13 23:35:27 -0500

    test

--- src/labs_examples/arrays/labs/Exercise_07.java
+++ src/labs_examples/arrays/labs/Exercise_07.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  *      Please demonstrate how to create an ArrayList, populate an array list, access elements within an ArrayList.
  *      Also take a moment to explore the many methods that are available to you when you use an ArrayList. By simply
  *      typing the dot operator (".") after the ArrayList object that you create. You should see a menu pop up that
- *      shows a list of methods.
+ *      shows a list of methods..
  *
  */
 public class Exercise_07 {
