aed2c1a5da1739c733a0263a88a690ae264ba143
utstudent <talhakaran@utexas.edu>
2022-09-02 23:12:18 -0500

    some edit

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
@@ -4,7 +4,7 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
  * Run the class below and study the output. What prints and why?
  *
  * Your answer: 10 is printed because 10 is in the parent class and all the variables and methods are inherited in class B.
- * If we wanted to have print 20, then we would need to override the variable?.
+ * If we wanted to have print 20, then we would need to override the variable?
  */
 class A {
     int i = 10;
