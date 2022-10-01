e033da1cb93149c7e0c008a428f7294b7bc24035
utstudent <talhakaran@utexas.edu>
2022-09-30 20:56:41 -0500

    stock app

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/StockApp/StockControllerClass.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/StockApp/StockControllerClass.java
@@ -7,12 +7,14 @@ public class StockControllerClass {
         Ledger ledger = new Ledger();
 
         Stock apple = new Stock("Apple", "AAPL", 100);
-        Stock tesla = new Stock("Tesla", "TESL", 150);
+        Stock tesla = new Stock("Tesla", "TSLA", 150);
         Stock microsoft = new Stock("Microsoft", "MSFT", 125);
 
         ledger.addTransaction(new Transaction(apple, 100, 5, true));
         ledger.addTransaction(new Transaction(apple, 110, 3, true));
         ledger.addTransaction(new Transaction(apple, 120, 7, true));
+
+        ledger.addTransaction(new Transaction(tesla, 270, 3, true));
 
 
     }
