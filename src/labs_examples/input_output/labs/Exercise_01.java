637d5fcadf5ccffcb19a456297f9db54f42028bb
utstudent <talhakaran@utexas.edu>
2022-10-02 23:18:42 -0500

    IO exercises

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -9,10 +11,28 @@ package labs_examples.input_output.labs;
  *
  */
 
-class Example {
+class Exercise_1 {
     public static void main(String[] args) {
 
+        String fileReadPath = "src/labs_examples/input_output/files/KtoD.txt";
+        String fileWritePath = "src/labs_examples/input_output/files/KtoD_copy.txt";
 
+        try (BufferedOutputStream bfOut = new BufferedOutputStream(new FileOutputStream(fileWritePath));
+             BufferedInputStream bfIn = new BufferedInputStream(new FileInputStream(fileReadPath))){
+
+            byte[] buffer = new byte[5];
+            int bytesRead = 0;
+
+            while ((bytesRead = bfIn.read(buffer)) != -1) {
+                String str  = new String(buffer,0, bytesRead);
+
+                bfOut.write(buffer,0, bytesRead);
+                System.out.println(str);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- src/labs_examples/input_output/labs/Exercise_02.java
+++ src/labs_examples/input_output/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 2: File encryption
  *
@@ -7,8 +9,30 @@ package labs_examples.input_output.labs;
  *      -For example, change every 'a' to '-' and every 'e' to '~' .
  *      -Make sure you close the connections to both files.
  *
- *      Then, ead back the encrypted file using the BufferedReader and
+ *      Then, read back the encrypted file using the BufferedReader and
  *      print out the unencrypted version. Does it match the original file?
  *
  */
+
+class Exercise_2 {
+
+    public static void main(String[] args) throws IOException{
+
+        String s;
+
+        String fileReadPath = "src/labs_examples/input_output/files/KtoD.txt";
+        String fileWritePath = "src/labs_examples/input_output/files/KtoD_2.txt";
+
+        // Create and use a FileReader wrapped in a BufferedReader.
+        try (BufferedReader br = new BufferedReader(new FileReader(fileReadPath));
+                BufferedWriter bw = new BufferedWriter(new FileWriter(fileReadPath)))
+        {
+            while((s = br.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch(IOException exc) {
+            System.out.println("I/O Error: " + exc);
+        }
+    }
+}
 
