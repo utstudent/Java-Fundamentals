d8e9bad89813d29a68b87c8b905fc28cb4c585dc
utstudent <talhakaran@utexas.edu>
2022-10-11 15:59:00 -0500

    multithread exercises

--- src/labs_examples/multi_threading/labs/Exercise_02.java
+++ src/labs_examples/multi_threading/labs/Exercise_02.java
@@ -5,3 +5,57 @@ package labs_examples.multi_threading.labs;
  *
  *      Create an application that creates a Thread using the Thread class
  */
+
+class Exercise_02 {
+    public static void main(String[] args) {
+        FirstThread thread1 = new FirstThread("Thread 1");
+        thread1.setPriority(10);
+
+        FirstThread thread2 = new FirstThread("Thread 2");
+        thread2.setPriority(5);
+        FirstThread thread3 = new FirstThread("Thread 3");
+        thread3.setPriority(1);
+
+        SecondThread thread4 = new SecondThread("Thread 4");
+        SecondThread thread5 = new SecondThread("Thread 5");
+        SecondThread thread6 = new SecondThread("Thread 6");
+    }
+}
+
+class FirstThread extends Thread{
+
+    FirstThread(String name){
+        super(name);
+        start();
+    }
+
+    @Override
+    public void run(){
+        System.out.println(getName() + " starting");
+        try {
+            for(int count=0; count < 5; count++) {
+                Thread.sleep(400);
+                System.out.println("In " + getName() +
+                        ", count is " + count);
+            }
+        }
+        catch(InterruptedException exc) {
+            System.out.println(getName() + " interrupted.");
+        }
+        System.out.println(getName() + " finished");
+    }
+}
+
+class SecondThread extends Thread{
+
+    SecondThread(String name){
+        super(name);
+        start();
+    }
+
+    @Override
+    public void run(){
+        System.out.println(getName() + " starting");
+    }
+}
+
