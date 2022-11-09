d1d18ec4e9aebd77639798ee00455b3dea81e4e2
utstudent <talhakaran@utexas.edu>
2022-11-08 23:43:24 -0600

    lambdas

--- src/labs_examples/lambdas/labs/Exercise_02.java
+++ src/labs_examples/lambdas/labs/Exercise_02.java
@@ -13,6 +13,9 @@ class Exercise_2{
     public static void main(String[] args) {
         FunctionExample();
         SupplierExample();
+        BiFunctionExample();
+        ConsumerExample();
+        PredicateExample();
     }
 
 
@@ -24,6 +27,29 @@ class Exercise_2{
     private static void SupplierExample(){
         Supplier<Boolean> isCool = () -> true;
         System.out.println(isCool.get());
+    }
+
+    private static void BiFunctionExample(){
+        BiFunction<Integer, Double, Double> biFunction = (x, y) -> {
+            Double sum =  x.doubleValue() + y;
+            return sum;
+        };
+        System.out.println(biFunction.apply(5,4.5));
+    }
+
+    private static void ConsumerExample(){
+        Consumer<String> consumer = (string) -> {
+            for (int i =0 ; i< string.length(); i++) {
+                System.out.println(string.charAt(i));
+            }
+        };
+        consumer.accept("Hello");
+    }
+
+    private static void PredicateExample(){
+        Predicate<String> predicate = s -> (s.startsWith("t"));
+
+        System.out.println("Name starts with t? " + predicate.test("talha"));
     }
 }
 
